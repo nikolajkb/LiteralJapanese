5dac1e0d1ce0715fa6ca55ed5194d2a4b489e66d
nbje <nbje@itu.dk>
2019-12-18 14:39:14 +0100

    framework and dict

--- src/Word.py
+++ src/Word.py
@@ -1,8 +1,12 @@
 from typing import List
 
 
+# specification
+# writings: kanji and kana writings of the word, used as search key
+# meanings: meanings of the word in English
+# grammar: type of word; verb, noun etc.
 class Word:
-    def __init__(self, writings, meanings):
+    def __init__(self, writings, meanings, grammar):
         self.writings: List[str] = writings
         self.meanings: List[str] = meanings
 
