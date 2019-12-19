5cc18757b97ea4cbfb9bda869f28bbb3a99974e8
nbje <nbje@itu.dk>
2019-12-19 13:48:03 +0100

    structure

---
+++ src/Tokenizer.py
@@ -0,0 +1,11 @@
+def _tokenize(word):
+    return[]  # todo
+
+
+def _merge_verb_endings(tokens):
+    return[]  # todo
+
+
+def get_tokens(word):
+    tokens = _tokenize(word)
+    return _merge_verb_endings(tokens)
--- src/Translator.py
+++ src/Translator.py
@@ -1,5 +1,8 @@
-from src import XmlReader
+from src import XmlReader, Tokenizer
 
 XmlReader.parse()
+
+words = Tokenizer.get_tokens("hello")
+error = Tokenizer._tokenize()
 
 
