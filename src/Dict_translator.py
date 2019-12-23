e3fdd17fb2f12dfd3b4c5fcc66b3024d8204bbc8
nbje <nbje@itu.dk>
2019-12-23 14:55:44 +0100

    working on the tokens

---
+++ src/Dict_translator.py
@@ -0,0 +1,2 @@
+def translate(tokens):
+    return []
--- src/Translator.py
+++ src/Translator.py
@@ -1,6 +1,13 @@
-from src import XmlReader, Tokenizer
+from src import Tokenizer, Dict_translator
 
-Tokenizer.get_tokens("家を出ようとしたら、トムが私に電話してきた。")
+
+def translate(text):
+    tokens = Tokenizer.get_tokens(text)
+    translations = Dict_translator.translate(tokens)
+    return translations
+
+
+Tokenizer.get_tokens("残ったものはなにひとつありませんでした。 ")
 Tokenizer.get_tokens("日曜日に出社したので月曜日が代休だった")
 Tokenizer.get_tokens("彼女は登校前によく髪を洗ったものだった。")
 Tokenizer.get_tokens("七夕は漫画によく出てくるので、私もそこそこ知っています。")
