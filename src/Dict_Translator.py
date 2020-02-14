51e91489d88d44d50ddf3728ca6fb805bd2761cd
nbje <nbje@itu.dk>
2020-02-14 17:58:15 +0700

    matching of special cases

--- src/Dict_Translator.py
+++ src/Dict_Translator.py
@@ -6,11 +6,48 @@ def translate(tokens):
     dicti = XmlReader().get_dict()
     translation = []
     for token in tokens:
-        eng = dicti.get(token.root)
-        if eng is None:
-            eng = Word.make_empty()
-            eng.meanings.append("ERROR")
         jp = token.word
-        translation.append((jp, eng.meanings[0]))
+        special = match_special(jp)
+        if special:
+            translation.append((jp, special))
+        else:
+            eng = dicti.get(token.root)
+            if eng is None:
+                eng = Word.make_empty()
+                eng.meanings.append("ERROR")
+            translation.append((jp, eng.meanings[0]))
 
     return translation
+
+
+def match_special(token):
+    return {
+        "を": "<o>",
+        "に": "<ni>",
+        "で": "<de>",
+        "の": "<no>",
+        "は": "<wa>",
+        "と": "<to>",
+        "も": "<mo>",
+        "ので": "so",
+        "か": "?",
+        "て": "<te>",
+        "。": ".",
+        "、": ",",
+        "「": "\"",
+        "」": "\"",
+        "～": "-",
+        "･･･": "...",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>",
+        "": "<>"
+    }.get(token, None)
