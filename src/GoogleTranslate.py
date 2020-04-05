c430494de29e12fd9fbc4627bb90ee08cbce09a9
nbje <nbje@itu.dk>
2020-04-05 14:36:09 +0200

    cleanup

--- src/GoogleTranslate.py
+++ src/GoogleTranslate.py
@@ -1,6 +1,5 @@
-from google.cloud import translate_v2
 import six
-import Credentials
+from google.cloud import translate_v2 as translate
 
 
 def translate_google(text):
@@ -8,21 +7,12 @@ def translate_google(text):
 
 
 def _translate_text(target, text):
-    # [START translate_translate_text]
-    """Translates text into the target language.
-    Target must be an ISO 639-1 language code.
-    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
-    """
-    from google.cloud import translate_v2 as translate
     translate_client = translate.Client()
 
     if isinstance(text, six.binary_type):
         text = text.decode('utf-8')
 
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
     result = translate_client.translate(
         text, target_language=target, source_language="ja", format_="text")
 
     return result['translatedText']
-    # [END translate_translate_text]
