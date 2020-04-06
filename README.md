1af4902998ddc5e6ad2e7f5082cf543b656a4143
nbje <nbje@itu.dk>
2020-04-06 17:35:34 +0200

    made setup.py, not tested

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # LitteralJapaneseTranslation
 To run the project:
-1. install sudachipy   
-pip install SudachiPy  
+1. run setup.py
+1. install sudachipy dictionary \
 pip install https://object-storage.tyo2.conoha.io/v1/nc_2520839e1f9641b08211a5c85243124a/sudachi/SudachiDict_core-20191224.tar.gz
 2. run Translator.py
 3. use -h argument to display available commands
-
-must be run as admin on Windows
---
+++ src/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='LiteralJapaneseTranslation',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='https://github.itu.dk/nbje/LiteralJapaneseTranslation',
+    license='GNU GPLv3',
+    author='Nikolaj Bjerregaard',
+    author_email='nbje@itu.dk',
+    description='Grammar preserving Japanese to English translation',
+    install_requires=[
+        "sudachipy",
+        "pickledb",
+        "six",
+        "google-cloud-translate==2.0.0",
+        "nltk",
+        "pykakasi",
+        "japanese-numbers-python",
+        "millify",
+        "colored",
+    ]
+)
