7f53549e0fb81f68bce9b864b95e09de4b0e0609
nbje <nbje@itu.dk>
2020-04-10 17:59:41 +0200

    refactor Paraphrase

--- src/Paraphrase.py
+++ src/Paraphrase.py
@@ -1,16 +1,25 @@
 import os
 import re
+from enum import Enum
 from Database import Database
 
 
+class Size(Enum):
+    SMALL = "ppdb-2.0-s-all"
+    MEDIUM = "ppdb-2.0-m-all"
+    LARGE = "ppdb-2.0-l-all"
+
+
+PPDB_SIZE = Size.SMALL
+
 def _parse_ppdb():
     print("loading paraphrase database")
-    database = Database("ppdb_small.db")
+    database = Database(PPDB_SIZE.value+".db")
     if database.is_empty():
         print("database not found")
         print("creating database, this might take a while...")
         file_dir = os.path.dirname(os.path.realpath('__file__'))
-        ppdb_path = os.path.join(file_dir, "..", "data", "PPDB", "ppdb-2.0-s-all")
+        ppdb_path = os.path.join(file_dir, "..", "data", "PPDB", PPDB_SIZE.value)
         ppdb = open(ppdb_path, "r")
 
         line = ppdb.readline()
