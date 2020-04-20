ff933897516e613a4b2d4f6497c37c4df6b5b839
nbje <nbje@itu.dk>
2020-04-20 16:09:51 +0200

    textual representation of translations

--- src/Translator.py
+++ src/Translator.py
@@ -12,6 +12,12 @@ class Translation:
         self.english = english
         self.token = token
 
+    def __repr__(self):
+        return "(" + self.japanese + " | " + self.english + ")"
+
+    def __str__(self):
+        return "(" + self.japanese + " | " + self.english + ")"
+
 
 def translate(tokens, translation=None):
     translations = []
